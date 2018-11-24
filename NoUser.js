7429ff20b4bd4341c0f740b602afbdff3c69cf73
yves <magneticchen@gmail.com>
2018-11-25 04:09:56 +0800

    first commit

---
+++ NoUser.js
@@ -0,0 +1,145 @@
+// NoUser.js
+// Description:
+// "NoUser.js" is a advanced user system for NoService.
+// Copyright 2018 NOOXY. All Rights Reserved.
+
+let Utils;
+'use strict';
+
+const USER_MODEL_NAME = 'User';
+// the nouser module
+function NoUser() {
+  let _model_module;
+  let _user_model;
+  let countries;
+
+  // import database from specified path
+  this.importModel = (model, callback)=> {
+    _model_module = model;
+    model.exist(USER_MODEL_NAME, (err, has_model)=> {
+      if(err) {
+        callback(err);
+      }
+      else if(!has_model) {
+        model.define(USER_MODEL_NAME, {
+          model_type: "Object",
+          do_timestamp: true,
+          model_key: "userid",
+          structure: {
+            userid: 'VARCHAR(255)',
+            email: 'VARCHAR(320)',
+            gender: 'VARCHAR(1)',
+            address: 'TEXT',
+            phonenumber: 'VARCHAR(50)',
+            birthday: 'DATE',
+            country: 'VARCHAR(160)',
+            aboutme: 'TEXT'
+          }
+        }, (err, user_model)=> {
+          _user_model = user_model;
+          callback(err);
+        });
+      }
+      else {
+        model.get(USER_MODEL_NAME, (err, user_model)=> {
+          _user_model = user_model;
+          callback(false);
+        });
+      }
+    });
+  };
+
+  this.importUtils = (utils)=> {
+    Utils = utils;
+  };
+
+  // import countries by list
+  this.importCountries = (list) => {
+    countries = list;
+  };
+
+  this.getUserMeta = (userid, callback) => {
+    _user_model.get(userid, (err, user) => {
+      if(user) {
+        let user_meta = {
+          userid: user.userid,
+          email: user.email,
+          gender : user.gender,
+          phonenumber : user.phonenumber,
+          birthday : user.birthday,
+          country : user.country,
+          address : user.address,
+          aboutme : user.aboutme
+        }
+        try {
+          user_meta.gender = user_meta.gender.replace('M', 'male');
+          user_meta.gender = user_meta.gender.replace('F', 'female');
+          user_meta.gender = user_meta.gender.replace('O', 'other');
+        }
+        catch(e) {
+
+        }
+        callback(false, user_meta);
+      }
+      else {
+        callback(false, null);
+      }
+
+    });
+  };
+
+  this.updateUser = (userid, jsondata, callback) => {
+    let pwdhash = null;
+    try {
+      jsondata.gender = jsondata.gender.replace('female', 'F');
+      jsondata.gender = jsondata.gender.replace('male', 'M');
+      jsondata.gender = jsondata.gender.replace('other', 'O');
+    }
+    catch (e) {
+
+    }
+    try{
+      if(!Utils.validateEmail(jsondata.email)) {
+        let err = new Error("Email invalid.");
+        callback(err);
+      }
+      else if(jsondata.gender!='M'&&jsondata.gender!='F'&&jsondata.gender!='O') {
+        let err = new Error("Gender invalid.");
+        callback(err);
+      }
+      else if(isNaN(Date.parse(jsondata.birthday))) {
+        let err = new Error("Birthday invalid.");
+        callback(err);
+      }
+      else if(!countries.includes(jsondata.country)) {
+        let err = new Error("Country invalid.");
+        callback(err);
+      }
+      else {
+        _user_model.update({
+          userid: userid,
+          email:  jsondata.email,
+          gender: jsondata.gender,
+          phonenumber: jsondata.phonenumber,
+          birthday: jsondata.birthday,
+          country: jsondata.country,
+          address: jsondata.address,
+          aboutme: jsondata.aboutme
+        }, callback);
+      }
+    }
+    catch (e) {
+      callback(e);
+    }
+  };
+
+  this.deleteUser = (userid, callback) => {
+    _user_model.remove(userid, callback);
+  };
+
+  this.close = () => {
+    _model_module.close();
+  };
+
+};
+module.exports = NoUser;
---
+++ entry.js
@@ -0,0 +1,158 @@
+// NoService/services/youservice/entry.js
+// Description:
+// "youservice/entry.js" description.
+// Copyright 2018 NOOXY. All Rights Reserved.
+'use strict';
+
+let NoUser = require('./NoUser');
+let fs = require('fs');
+
+function Service(Me, api) {
+  // Your service entry point
+  // Get the service socket of your service
+  let ss = api.Service.ServiceSocket;
+  // BEWARE! To prevent callback error crash the system.
+  // If you call an callback function which is not API provided. Such as setTimeout(callback, timeout).
+  // You need to wrap the callback funciton by api.SafeCallback.
+  // E.g. setTimeout(api.SafeCallback(callback), timeout)
+  let safec = api.SafeCallback;
+  // Please save and manipulate your files in this directory
+  let files_path = Me.FilesPath;
+  // Your settings in manifest file.
+  let settings = Me.Settings;
+
+  let country_list = Me.Settings.country_list;
+  let nouser = new NoUser();
+
+  nouser.importModel(api.Database.Model, (err)=> {
+  });
+  nouser.importUtils(api.Utils);
+  nouser.importCountries(country_list);
+
+
+  // Your service entry point
+  this.start = ()=> {
+    // JSONfunction is a function that can be defined, which others entities can call.
+    // It is a NOOXY Service Framework Standard
+    ss.def('createUser', (json, entityID, returnJSON)=>{
+      // Code here for JSONfunciton
+      // Return Value for JSONfunction call. Otherwise remote will not recieve funciton return value.
+      let json_be_returned = {
+        s: 'Succeessfully created.'
+      }
+      // First parameter for error, next is JSON to be returned.
+      if (json.pw != json.cp) {
+        json_be_returned.e = true;
+        json_be_returned.s = 'Error: password not match.';
+        returnJSON(false, json_be_returned);
+      }
+      else {
+        api.Authenticity.createUser(json.un, json.dn, json.pw, 1, json.dt, json.fn, json.ln, (err)=>{
+          if(err) {
+            json_be_returned.e = true;
+            json_be_returned.s = err.toString();
+          }
+          returnJSON(false, json_be_returned);
+        });
+      }
+    });
+
+    ss.def('returnUserMeta', (json, entityID, returnJSON)=>{
+      api.Service.Entity.getEntityOwner(entityID, (err, username)=> {
+        api.Authorization.Authby.Token(entityID, (err, valid)=>{
+          if(valid) {
+            api.Authenticity.getUserMeta(username, (err, meta1)=>{
+              api.Authenticity.getUserID(username, (err, userid) => {
+                nouser.getUserMeta(userid, (err, meta2)=>{
+                  returnJSON(false, Object.assign({}, meta1, meta2));
+                })
+              });
+            })
+          }
+          else {
+            returnJSON(false, {});
+          }
+        });
+      })
+    });
+
+    // JSONfunction is a function that can be defined, which others entities can call.
+    // It is a NOOXY Service Framework Standard
+    ss.def('updateUser', (json, entityID, returnJSON)=>{
+      api.Service.Entity.getEntityOwner(entityID, (err, username)=> {
+        let json_be_returned = {
+          s: 'Succeessfully updated.'
+        }
+        api.Authorization.Authby.Password(entityID, (err, valid)=>{
+          if(valid) {
+
+            for(let i in json) {
+              if(json[i] == '') {
+                json[i] = null;
+              }
+            }
+            // First parameter for error, next is JSON to be returned.
+            if (json.pw != json.cp) {
+              json_be_returned.e = true;
+              json_be_returned.s = 'Error: password not match.';
+              returnJSON(false, json_be_returned);
+            }
+            else {
+              api.Authenticity.updatePassword(username, json.pw, (err)=>{
+                if(err&&json.pw!=null) {
+                  json_be_returned.e = true;
+                  json_be_returned.s = err.toString();
+                  returnJSON(false, json_be_returned);
+                }
+                else {
+                  if(json.firstname != null && json.lastname!= null) {
+                    api.Authenticity.updateName(username, json.firstname, json.lastname, (err)=>{
+                      if(err) {
+                        json_be_returned.e = true;
+                        json_be_returned.s = err.toString();
+                        returnJSON(false, json_be_returned);
+                      }
+                      else {
+                        api.Authenticity.getUserID(username, (err, userid) => {
+                          nouser.updateUser(userid, json, (err)=>{
+                            if(err) {
+                              json_be_returned.e = true;
+                              json_be_returned.s = err.toString();
+                            }
+                            returnJSON(false, json_be_returned);
+                          });
+                        });
+                      }
+                    });
+                  }
+                  else {
+                    json_be_returned.e = true;
+                    json_be_returned.s = 'Error: Please enter your name.';
+                    returnJSON(false, json_be_returned);
+                  }
+                }
+              });
+
+            }
+          }
+          else {
+            json_be_returned.e = true;
+            json_be_returned.s = 'Error: Auth failed.';
+            returnJSON(false, json_be_returned);
+          }
+        });
+      });
+
+    });
+
+    ss.on('close', (entityID, callback) => {callback(false)});
+  }
+
+  // If the daemon stop, your service recieve close signal here.
+  this.close = ()=> {
+    nouser.close();
+  }
+}
+
+// Export your work for system here.
+module.exports = Service;
